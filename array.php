8b7172e5548afb6b740ee581077cdad96d03ec29
Harendar <harendar04@gmail.com>
2021-05-31 12:08:16 +0530

    Second commit

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php 
+ $array = [44, 12, 34, 47, 2, 15];
+$key= 0;
+for( $x = 0; $x <= 100; $x+=10  ){
+    
+}
+foreach( $array as $key => $value){    
+     $value;
+     $array[$key];
+     if( $array[$key] >= $value ){
+      echo $value ."<br>";
+   }
+ }
+?>
